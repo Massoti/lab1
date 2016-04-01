0229127d7bcb4b1c794fbd19fd1be9ad96f1e88c
Massoti <225967@student.pwr.edu.pl>
2016-04-01 09:44:32 +0200

    Gotowe

--- main.cpp
+++ main.cpp
@@ -8,16 +8,42 @@ void drawCharSquare(char **square, int n);
 int main(int argc, char **argv)
 {
 	int n;
-	drawCharSquare(charSquare(n), n);	
+	
+	n=atoi(argv [1]);
+
+	srand(atoi(argv[2]));
+
+	drawCharSquare(charSquare(n), n);
+
 }
 
 char **charSquare(int n)
 {
 	char **square;
+
+	square = new char * [n];
+
+	for(int i=0; i<n; i++)
+		square [i] = new char [n];
+
+	for(int i=0; i<n*n; i++)
+		square [i/n] [i%n] = rand()%('z'-'a'+1)+'a';
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+
+	for(int i=0; i<n*n; i++){
+
+		printf("%c", square [i/n] [i%n]);
+
+		if ((i+1)%n==0)
+			printf("\n");
+
+	}
+
+	delete[] square;
 
 }
